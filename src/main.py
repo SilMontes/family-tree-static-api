046a4e27d88e1e63480eb1e311505520f6ef6206
SilMontes <Silmontes2001@gmail.com>
2021-04-10 00:45:37 +0000

    Missing grandparents

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, Person
+from models import db, Person, PeopleRelationship
 import datetime 
 #from models import Person
 
@@ -30,72 +30,127 @@ def handle_invalid_usage(error):
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
-
+#------GET ALL PEOLPLE--------------------------------------------
 @app.route('/people', methods=['GET'])
 def get_all_people():
     people=Person.query.all()
     all_people=list(map(lambda person:person.serialize(),people))
 
     if all_people==[]:
-        return jsonify('Nothing here!')
+        return jsonify('Nothing here!'),200
     else:
         return jsonify(all_people), 200
+#------------------GET ALL RELATIONSHIPS-----------------
+@app.route('/relationships', methods=['GET'])
+def get_relationship():
+    relationships=PeopleRelationship.query.all()
+    all_relationships=list(map(lambda item:item.serialize(),relationships))
 
+    if all_relationships == []:
+        return jsonify('Nothing here!'),200
+    else:
+        return jsonify(all_relationships),200
+
+#----------ADD A PERSON----------------------------------------
 @app.route('/newperson',methods=['POST'])
 def create_person():
     request_body=request.get_json()
+    if request_body == {} or request_body == [] or request_body==None:
+        return jsonify('Empty Request'),400
     name=request.json.get('name',None)
     last_name=request.json.get('last_name',None)
     age=request.json.get('age',None)
+
     if not name:
-        return jsonify({'msg':'Name required'}),400
-    if not last_name:
-        return jsonify({'msg':'Last Name required'}),400
-    if not age:
-        return jsonify({'msg':'Age of birth required'}),400
+        return jsonify({'msg':'Name Required'}),400
+    elif not last_name:
+        return jsonify({'msg':'Last Name Required'}),400
+    elif not age:
+        return jsonify({'msg':'Age Required'}),400
+    else:
+        newPerson=Person(name=name,last_name=last_name,age=age)
+        db.session.add(newPerson)
+        db.session.commit()
     
-    newPerson = Person(name=name, last_name=last_name,age=age)
-    db.session.add(newPerson)
-    db.session.commit()
     return jsonify(request_body),200
 
-@app.route('/person/<int:person_id>')
-def get_person(person_id):
-    person=Person.query.get(id)
-
-    ############### Children ###############
-    child_id =Person.query.filter_by(mother_id=person_id)
-    print('child1',child_id)
-    if not child_id:
-        child_id2 =Person.query.filter_by(father_id=person_id)
-        print('child',child_id2)
-
-    ############ Parents ######################
-    mother_id=person.mother_id
-    father_id=person.father_id
-    mother=Person.query.filter_by(mother_id=mother_id)
-    father=Person.query.filter_by(father_id=father_id)
-
-    print('parents',mother,father)
-    ############### Grand Parents ##############
-                        ########### Mother Parents
-    grand_ma_id_mother= mother.mother_id
-    grand_pa_id_mother= mother.father_id
-    grand_ma_mother=Person.query.filter(mother_id=grand_ma_id_mother)
-    garnd_pa_mother =Person.query.filter(father_id=grand_pa_id_mother)
-
-    print('gandMother',grand_ma_mother,garnd_pa_mother)
-                        ########### Father Parents
-    grand_ma_id_father= father.mother_id
-    grand_pa_id_father= father.father_id
-    grand_ma_father=Person.query.filter(mother_id=grand_ma_id_father)
-    garnd_pa_father =Person.query.filter(father_id=grand_pa_id_father)
-    print('gandFather',grand_ma_father,garnd_pa_father)
+#--------------CREATE RELATIONSHIP---------------------------------------------
+@app.route('/create_relationship', methods=['POST'])
+def create_relationship():
+    request_body=request.get_json()
+    if request_body == {} or request_body==[] or request_body==None:
+        return jsonify('Empty Request'),400
+    person_id=request.json.get('person_id',None)
+    mother_id=request.json.get('mother_id')
+    father_id=request.json.get('father_id')
+    if not person_id:
+        return jsonify('Person_id required'),400
+    already_assigned_relationship=PeopleRelationship.query.filter_by(person_id=person_id).first()
+    if already_assigned_relationship:
+        return jsonify('Already added'),400
     
+    person_parents=PeopleRelationship(person_id=person_id,mother_id=mother_id,father_id=father_id)
+    db.session.add(person_parents)
+    db.session.commit()
+    
+    return jsonify(request_body),200
 
+#------------------------GET A SPECIFIED PERSON----------------------------------------
+@app.route('/person/<int:person_id>', methods=['GET'])
+def get_specified_person(person_id):
 
-    return jsonify(person.serialize())
+    pieces_of_information={} #EVERY piece of information will be added to this one. Finally, it will be return
+    
+    specified_person=Person.query.filter_by(id=person_id).first()
+    print(specified_person)
+    if not specified_person:
+        return jsonify({'msg':'This person does not exists'}),400
 
+    pieces_of_information['personal information']=specified_person.serialize()
+
+     # Get all relationships
+    relationships=PeopleRelationship.query.all()
+    all_relationships=list(map(lambda item:item.serialize(),relationships))
+    # get all people
+    people=Person.query.all()
+    all_people=list(map(lambda person:person.serialize(),people))
+
+    #!!!!!!!!!!!!!!!!CHILDREN!!!!!!!!!!!!!
+    # child id
+    children_id=[]
+    parents_id=[]
+    for items in all_relationships:
+        if items['mother_id']!= None and items['mother_id'] == person_id:
+            children_id.append(items['person_id'])
+        elif items['father_id']!= None and items['father_id'] == person_id:
+            children_id.append(items['person_id'])
+        elif items['person_id']==person_id: ##append parent id
+            parents_id.append({'mother':items['mother_id']})
+            parents_id.append({'father':items['father_id']})
+
+    children_information=[]
+    for eachId in children_id:
+        for person in all_people:
+            if person['id'] == eachId:
+                children_information.append(person)
+    
+    pieces_of_information['children']=children_information
+
+    #!!!!!!!!!!!!!!!!!!! Parent !!!!!!!!!!!!!!!!!!!!!!!!!!
+    parents_information={}
+    for eachParentId in parents_id:
+        for eachperson in all_people:
+            if 'mother' in eachParentId:
+                if eachParentId['mother']==eachperson['id']: ###padres
+                    parents_information['mother']=eachperson
+            elif 'father' in eachParentId:
+                if eachParentId['father']==eachperson['id']: ###padres
+                    parents_information['father']=eachperson
+    pieces_of_information['parents information']=parents_information
+
+    return jsonify(pieces_of_information),200
+
+#---------------------------
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
--- src/models.py
+++ src/models.py
@@ -3,13 +3,11 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class Person(db.Model):
+    __tablename__:'person'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), unique=False, nullable=False)
     last_name = db.Column(db.String(80), unique=False, nullable=False)
     age = db.Column(db.Integer, unique=False, nullable=False)
-    father_id = db.Column(db.Integer, unique=False,nullable=True)
-    mother_id = db.Column(db.Integer, unique=False, nullable=True)
-
     def __repr__(self):
         return '<Person %r>' % self.name
 
@@ -18,9 +16,23 @@ class Person(db.Model):
             "id": self.id,
             "name": self.name,
             "last_name": self.last_name,
-            "age":self.age,
-            "father_id": self.father_id,
-            "mother_id": self.mother_id
-            # do not serialize the password, its a security breach
+            "age":self.age
         }
 
+class PeopleRelationship(db.Model):
+    __tablename__='PeopleRelationship'
+    id = db.Column(db.Integer, primary_key=True)
+    person_id = db.Column(db.Integer,db.ForeignKey('person.id'),nullable=False)
+    father_id = db.Column(db.Integer,db.ForeignKey('person.id'),nullable=True)
+    mother_id = db.Column(db.Integer,db.ForeignKey('person.id'),nullable=True)
+
+    def __repr__(self):
+        return '<Relationship %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "person_id": self.person_id,
+            "father_id": self.father_id,
+            "mother_id":self.mother_id
+        }
